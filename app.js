faf0e54b0db6df6cd81002347c6d019e9258ef24
eruc <eruc101010@gmail.com>
2022-01-04 16:26:50 +0800

    feat: Add show page to display restaurant detail information.

--- app.js
+++ app.js
@@ -2,19 +2,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
+
 //載入handlebars
 const exhdbs = require('express-handlebars')
+
 //設定使用handlebars
 app.engine('handlebars', exhdbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
 //載入restaurant.json
 let restaurantListJson = require('./restaurant.json')
 let restaurantList = restaurantListJson.results
+
 //使用public設定
 app.use(express.static('public'))
 //get取得頁面
 app.get('/', (req, res) => {
   res.render('index', { restaurantList })
+})
+//顯示餐廳詳細資料
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.find(element => element.id.toString() === req.params.restaurant_id)
+  res.render('show', {restaurant})
 })
 //設定port
 app.listen(port, () => {

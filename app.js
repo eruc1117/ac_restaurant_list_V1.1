f40a89c28d38297d12ce84702dd1ddb622d38f2b
eruc <eruc101010@gmail.com>
2022-01-04 15:56:15 +0800

    feat: Add index page. Show index page by express-handlebars. Dowlond restaurant.json.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-const express = require('expres
+//設定express環境
+const express = require('express')
+const app = express()
+const port = 3000
+//載入handlebars
+const exhdbs = require('express-handlebars')
+//設定使用handlebars
+app.engine('handlebars', exhdbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+//使用public設定
+app.use(express.static('public'))
+//get取得頁面
+app.get('/', (req, res) => {
+  res.render('index')
+})
+//設定port
+app.listen(port, () => {
+  console.log(`localhost:${port}`)
+})

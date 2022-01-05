305dadf8334030ddfe2c6884396e979855f48b5a
eruc <eruc101010@gmail.com>
2022-01-05 20:24:30 +0800

    refactor: Add .toLowerCase() in keyword variable.

--- app.js
+++ app.js
@@ -32,9 +32,9 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
 //搜索功能
 app.get('/search', (req, res) => {
 
-  const keyword = modules.removeBlank(req.query.keyword)
+  const keyword = modules.removeBlank(req.query.keyword).toLowerCase()
   const restaurantList = restaurantListJson.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.name_en.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+    return restaurant.name.toLowerCase().includes(keyword) || restaurant.name_en.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes()
     //思考了一下關於這裡name跟name_en的優先順序，首先filter會篩選出true的項目，由於name在||前，當name符合keyword時先回傳值，此時不會對name_en進行驗證，所以優先程度上是以name為優先
   })
   restaurantList.length === 0 ? res.render('nonSearchResult') : res.render('index', { restaurantList, keyword })

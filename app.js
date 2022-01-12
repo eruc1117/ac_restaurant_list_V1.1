013ca05a883efc3c976a36909794749d081cb602
eruc <eruc101010@gmail.com>
2022-01-12 13:26:44 +0800

    refactor: seach functin change to seach database.

--- app.js
+++ app.js
@@ -96,14 +96,14 @@ app.post('/restaurants/delete/:id', (req, res) => {
 
 //搜索功能
 app.get('/search', (req, res) => {
-  const searchResult = req.query.keyword
   const keyword = modules.removeBlank(req.query.keyword).toLowerCase()
-  const restaurantList = restaurantListJson.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword) || restaurant.name_en.toLowerCase().includes(keyword) || restaurant.category.toLowerCase().includes()
-    //思考了一下關於這裡name跟name_en的優先順序，首先filter會篩選出true的項目，由於name在||前，當name符合keyword時先回傳值，此時不會對name_en進行驗證，所以優先程度上是以name為優先
-  })
-  restaurantList.length === 0 ? res.render('nonSearchResult', { searchResult }) : res.render('index', { restaurantList, searchResult })
+  const reg = new RegExp(keyword, 'i')
+  restaurantModel.find({ $or: [{ name: reg }, { name_en: reg }, { category: reg }] })
+    .lean()
+    .then(restaurantList => restaurantList.length === 0 ? res.redirect('/') : res.render('index', { restaurantList }))
+    .catch(error => console.log('error:' + error))
 })
+
 
 //設定port
 app.listen(port, () => {

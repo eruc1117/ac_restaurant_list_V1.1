e10c6ac2b04950eecaba4e6d042d2a9462dec5c5
eruc <eruc101010@gmail.com>
2022-02-20 11:49:50 +0800

    feat: add user authentication in todo and home routers

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,23 @@ const express = require('express')
 const router = express.Router()
 // 準備引入路由模組
 
+const { authenticator } = require('../middleware/auth')
+
 // 引入 home 模組程式碼
 const home = require('./modules/home')
-// 將網址結構符合 / 字串的 request 導向 home 模組 
-router.use('/', home)
+
 
 // 引入 restaurantList 模組程式碼
 const restaurantList = require('./modules/restaurantList')
 // 將網址結構符合 / 字串的 request 導向 home 模組 
-router.use('/user', restaurantList)
+router.use('/user', authenticator, restaurantList)
 
 const crud = require('./modules/crud')
 // 將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組 
-router.use('/crud', crud)
+router.use('/crud', authenticator, crud)
 
-
+// 將網址結構符合 / 字串的 request 導向 home 模組 
+router.use('/', home)
 
 // 匯出路由器
 module.exports = router

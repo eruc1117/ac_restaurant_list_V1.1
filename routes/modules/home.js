25d2cde8928a931db2067d58c1ade0cef79a2729
eruc <eruc101010@gmail.com>
2022-02-20 11:02:33 +0800

    feat: add user registration feature

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,16 +1,61 @@
-// 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
-const modules = require('../../models/modules')
+const User = require('../../models/user')
+// 引用 passport
+
 
 router.get('/', (req, res) => {
   res.render('login')
 })
-
+// routes/modules/users.js
+router.post('/login', (req, res) => {
+})
 router.get('/register', (req, res) => {
   res.render('register')
 })
+router.post('/register', (req, res) => {
+  // 取得註冊表單參數
+  const { name, email, password, confirmPassword } = req.body
+  const userData = req.body
+  // 檢查使用者是否已經註冊
+  try {
+    repeatCheck(userData)
+      .then(test => {
+        if (test.repeat) {
+          res.render('register', {
+            name,
+            email,
+            password,
+            confirmPassword
+          })
+        } else {
+          res.redirect('/')
+        }
+      })
+  } catch (err) {
+    console.log(err)
+  }
+
+})
 
 
-// 匯出路由模組
 module.exports = router
+
+async function repeatCheck(userData) {
+  let result = await User.findOne({ email: userData.email })
+  if (result) {
+    console.log('User already exists.')
+    return {
+      data: result, repeat: true
+    }
+  } else {
+    await User.create({
+      name: userData.name,
+      email: userData.email,
+      password: userData.password
+    })
+    return {
+      data: userData, repeat: false
+    }
+  }
+}

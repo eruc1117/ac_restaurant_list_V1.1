0c8b1c8940e37158090f44c94d7395a88c8c6bcc
eruc <eruc101010@gmail.com>
2022-02-20 11:54:51 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -48,6 +48,12 @@ app.use(express.static('public'))
 //表單資料處理
 app.use(express.urlencoded({ extended: true }))
 usePassport(app)
+app.use((req, res, next) => {
+  // 你可以在這裡 console.log(req.user) 等資訊來觀察
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 // 將 request 導入路由器
 app.use(routes)
 

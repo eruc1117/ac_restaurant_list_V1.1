31cd9b818df37931004d06d26a13ac87d8531ab8
eruc <eruc101010@gmail.com>
2022-02-20 10:28:18 +0800

    feat: add register page.

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,10 +3,14 @@ const express = require('express')
 const router = express.Router()
 const modules = require('../../models/modules')
 
-
 router.get('/', (req, res) => {
   res.render('login')
 })
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 
 // 匯出路由模組
 module.exports = router
